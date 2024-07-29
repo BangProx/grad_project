54fd4ff4100960b71e031a9139931cfe5581085c
BangProx <stush0626@gmail.com>
2024-07-29 21:07:24 +0900

    add Allowed Host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-+!-6y9(i&6d2_3rnm+my%-921_mjt-k_fv-v5qk)g9ocr@^ye8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
